c858b80b43b2fdf2cc2f02b95dc78454b59a0045
Danilo Toriy <randomlittlest@gmail.com>
2026-03-22 19:42:57 -0300

    feat: criando a entidade Livro e adicionando README

---
+++ src/main/java/entity/LivroModel.java
@@ -0,0 +1,56 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="TBL_LIVRO")
+
+public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="Titulo_livro")
+    private String titulo;
+    private String autor;
+
+    @Column(name="Ano_de_publicacao")
+    private int anoPublicacao;
+
+    public LivroModel(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
